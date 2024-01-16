edbdd6dcc62865204996d5772ab2b406c077acc4
Ogunsanya James <oyindamolajames@gmail.com>
2024-01-16 19:19:28 +0100

    initial commit

---
+++ React_Series/01tut/src/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () => {
+
+    const headerStyle = 
+
+    return (
+        <header style= {{
+            backgroundColor: "burlywood",
+
+        }}>
+            <h1>Programming Languages</h1>
+        </header>
+    )
+}
+
+export default Header
+
